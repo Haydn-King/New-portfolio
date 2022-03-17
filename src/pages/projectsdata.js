40bb13867de7e684b6784e2cc16bda1b8469c11d
Haydn King <ahmillikin@gmail.com>
2022-03-16 18:24:08 -0700

    Added UE5FPS to Projects

--- src/pages/projectsdata.js
+++ src/pages/projectsdata.js
@@ -1,74 +1,81 @@
 const projectsdata = [
-  {
-    title: "React Store",
-    image: "./react-store-dotnet.png",
-    description:
-      "An ecommerce application developed using React, Node, Redux, DotNet and Heroku.",
-    link: "http://react-store-dotnet.herokuapp.com/",
-  },
-  {
-    title: "Parting Ways",
-    image: "./andrew.png",
-    description:
-      "Writing, recording, and performing live as the drummer for Parting Ways!",
-    link: "https://open.spotify.com/artist/63QirPBrFxjY1ZBzWlWBAP",
-  },
-  {
-    title: "Ethereum Marketplace",
-    image: "./eth-marketplace.png",
-    description:
-      "An Ethereum Marketplace developed using Javascript, Node, Next.JS and Vercel.",
-    link: "https://eth-marketplace-course.vercel.app/",
-  },
-  {
-    title: "Unreal Engine Shooter",
-    image: "./Ueshooter.png",
-    description:
-      "A Shooter game built with C++ and Unreal Engine.",
-    link: "https://drive.google.com/file/d/1bpBt0H788CQWeX6f8o8pGHEcbj9xqy9r/view?usp=sharing",
-  },
-  {
-    title: "Unity RPG Adventure Game",
-    image: "./RPGAdventure.png",
-    description:
-      "Simple RPG Adventure Game built with C# & Unity. One Map with a simple quest.",
-    link: "https://drive.google.com/file/d/1v52XPb_Jdbt-T4q4qt62zmzUAcZh9xKk/view?usp=sharing",
-  },
-  {
-    title: "React Events",
-    image: "./react-events.png",
-    description:
-      "An Events application developed using React, Node, Redux and Firebase",
-    link: "https://project-react-events.firebaseapp.com/",
-  },
-  {
-    title: "Crypto Price Updates",
-    image: "./crypto-price-updates.png",
-    description:
-      "Live Cryptocurrency price updates using CAD CoinGecko API",
-    link: "https://naughty-goldstine-dfb7af.netlify.app/",
-  },
-  {
-    title: "Crystal World Platform Game",
-    image: "./splash_screen.png",
-    description:
-      "A Simple Crystal World Platform game built using JavaScript and Phaser 3",
-    link: "https://tranquil-brushlands-50560.herokuapp.com/",
-  },
-  {
-    title: "Flappy Bird Clone",
-    image: "./flappybird.png",
-    description:
-      "Flappy Bird Clone built using JavaScript and Phaser 3",
-    link: "https://quizzical-shockley-072ea5.netlify.app/",
-  },
-  {
-    title: "Simple Platform Game",
-    image: "./platformer.png",
-    description:
-      "A Simple Platform game built using JavaScript and Phaser 3",
-    link: "https://wonderful-liskov-f195ab.netlify.app/",
-  },
+	{
+		title: "React Store",
+		image: "./react-store-dotnet.png",
+		description:
+			"An ecommerce application developed using React, Node, Redux, DotNet and Heroku.",
+		link: "http://react-store-dotnet.herokuapp.com/",
+	},
+	{
+		title: "Parting Ways",
+		image: "./andrew.png",
+		description:
+			"Writing, recording, and performing live as the drummer for Parting Ways!",
+		link: "https://open.spotify.com/artist/63QirPBrFxjY1ZBzWlWBAP",
+	},
+	{
+		title: "Ethereum Marketplace",
+		image: "./eth-marketplace.png",
+		description:
+			"An Ethereum Marketplace developed using Javascript, Node, Next.JS and Vercel.",
+		link: "https://eth-marketplace-course.vercel.app/",
+	},
+	{
+		title: "Unreal Engine Shooter",
+		image: "./Ueshooter.png",
+		description:
+			"A Shooter game built with C++ and Unreal Engine.",
+		link: "https://drive.google.com/file/d/1bpBt0H788CQWeX6f8o8pGHEcbj9xqy9r/view?usp=sharing",
+	},
+	{
+		title: "Unreal Engine 5 FPS Blueprints Only",
+		image: "./UE5FPS.JPG",
+		description:
+			"A FPS game built with Unreal Engine 5 and Blueprints.",
+		link: "https://drive.google.com/file/d/1o6yW9zJrK-dHfcFCIdATFCKC47YVdgK3/view?usp=sharing",
+	},
+	{
+		title: "Unity RPG Adventure Game",
+		image: "./RPGAdventure.png",
+		description:
+			"Simple RPG Adventure Game built with C# & Unity. One Map with a simple quest.",
+		link: "https://drive.google.com/file/d/1v52XPb_Jdbt-T4q4qt62zmzUAcZh9xKk/view?usp=sharing",
+	},
+	{
+		title: "React Events",
+		image: "./react-events.png",
+		description:
+			"An Events application developed using React, Node, Redux and Firebase",
+		link: "https://project-react-events.firebaseapp.com/",
+	},
+	{
+		title: "Crypto Price Updates",
+		image: "./crypto-price-updates.png",
+		description:
+			"Live Cryptocurrency price updates using CAD CoinGecko API",
+		link: "https://naughty-goldstine-dfb7af.netlify.app/",
+	},
+	{
+		title: "Crystal World Platform Game",
+		image: "./splash_screen.png",
+		description:
+			"A Simple Crystal World Platform game built using JavaScript and Phaser 3",
+		link: "https://tranquil-brushlands-50560.herokuapp.com/",
+	},
+	{
+		title: "Flappy Bird Clone",
+		image: "./flappybird.png",
+		description:
+			"Flappy Bird Clone built using JavaScript and Phaser 3",
+		link: "https://quizzical-shockley-072ea5.netlify.app/",
+	},
+	{
+		title: "Simple Platform Game",
+		image: "./platformer.png",
+		description:
+			"A Simple Platform game built using JavaScript and Phaser 3",
+		link: "https://wonderful-liskov-f195ab.netlify.app/",
+	},
 ];
 
 export default projectsdata;
